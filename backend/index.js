2c79a5fb416475d1a12c8f5d326ae240acd6358f
TEJASTATODE <tejastatode449@gmail.com>
2025-08-31 13:59:08 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'https://task-manager-coral-five.vercel.app/',
+        origin: 'https://task-manager-coral-five.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization']
     }
