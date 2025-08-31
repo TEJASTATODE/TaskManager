1be1ff3813adca1c57619d6f45cda0efbb7783c6
TEJASTATODE <tejastatode449@gmail.com>
2025-08-31 13:50:46 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://task-manager-coral-five.vercel.app/',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization']
     }
--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: ' https://taskmanager-344f.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
